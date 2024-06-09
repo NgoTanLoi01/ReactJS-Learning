03b9da7ac85055dca5abde365ed4b8bf2e72b615
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-06-09 22:59:24 +0700

    useEffect hook

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
 
+// 1.useEffect(callback)
+//  - Gọi callback mỗi khi component re-render
+//  - Gọi callback sau khi component thêm element và dom
+// 2.useEffect(callback, [])
+// -Chỉ gọi callback 1 lần sau khi component mounted
+// 3.useEffect(callback, [deps])
 
-function Content(){
-    return(
-        <h1>Xin chao tat ca moi nguoi</h1>
-    )
+//-----------------
+//1. Callback luôn được gọi sau khi component mounted
+
+function Content() {
+  const [title, setTitle] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((posts) => {
+        setPosts(posts);
+      });
+  }, []);
+
+  return (
+    <div>
+      <input 
+        value={title} 
+        onChange={(e) => setTitle(e.target.value)} 
+      />
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Content
+export default Content;
