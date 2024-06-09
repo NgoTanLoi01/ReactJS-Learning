06d085bacfeff95350d400361b5314391b293b65
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-06-09 22:23:51 +0700

    Mounted & Unmounted

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,36 +1,15 @@
 import { useState } from "react";
+import Content from "./Content";
+
+//Mounted / Unmounted
 
 function App() {
-  const [job, setJob] = useState("");
-  const [jobs, setJobs] = useState(() => {
-    const storageJobs = JSON.parse(localStorage.getItem("jobs"))
-
-    return storageJobs
-  });
-
-  const handleSubmit = () => {
-    setJobs((prev) => {
-      const newJobs = [...prev, job];
-
-      //Save to localStorage
-      const jsonJobs = JSON.stringify(newJobs);
-      localStorage.setItem("jobs", jsonJobs);
-
-      return newJobs;
-    });
-    setJob("");
-  };
+  const [show, setShow] = useState(false)
 
   return (
-    <div style={{ padding: 32 }}>
-      <input value={job} onChange={(e) => setJob(e.target.value)} />
-      <button onClick={handleSubmit}>Add</button>
-
-      <ul>
-        {jobs.map((job, index) => (
-          <li key={index}>{job}</li>
-        ))}
-      </ul>
+    <div style={{ padding: 20 }}>
+      <button onClick={() =>setShow(!show)}>Toggle</button>
+      {show && <Content />}
     </div>
   );
 }
---
+++ tiktok/src/Content.js
@@ -0,0 +1,9 @@
+
+
+function Content(){
+    return(
+        <h1>Xin chao tat ca moi nguoi</h1>
+    )
+}
+
+export default Content
