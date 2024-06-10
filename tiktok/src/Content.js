7108c72fc0482f58704a3112d24f1bd10cdd232a
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-06-10 22:37:15 +0700

    useEffect with dependencies

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -4,30 +4,48 @@ import { useEffect, useState } from "react";
 //  - Gọi callback mỗi khi component re-render
 //  - Gọi callback sau khi component thêm element và dom
 // 2.useEffect(callback, [])
-// -Chỉ gọi callback 1 lần sau khi component mounted
+//  -Chỉ gọi callback 1 lần sau khi component mounted
 // 3.useEffect(callback, [deps])
+//  - Callback sẽ được gọi lại mỗi khi deps thay đổi
 
 //-----------------
 //1. Callback luôn được gọi sau khi component mounted
 
+const tabs = ["posts", "comments", "albums"];
+
 function Content() {
   const [title, setTitle] = useState("");
   const [posts, setPosts] = useState([]);
+  const [type, setType] = useState(["posts"]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch(`https://jsonplaceholder.typicode.com/${type}`)
       .then((res) => res.json())
       .then((posts) => {
         setPosts(posts);
       });
-  }, []);
+  }, [type]);
 
   return (
     <div>
-      <input 
-        value={title} 
-        onChange={(e) => setTitle(e.target.value)} 
-      />
+      {tabs.map((tab) => (
+        <button
+          key={tab}
+          style={
+            type === tab
+              ? {
+                  color: "#fff",
+                  backgroundColor: "#333",
+                }
+              : {}
+          }
+          onClick={() => setType(tab)}
+        >
+          {tab}
+        </button>
+      ))}
+
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
       <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
