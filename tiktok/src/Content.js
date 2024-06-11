6caf9218af0f8c68225fbfdcf1b526545f310ee8
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-06-11 23:14:16 +0700

    useEffect with timer functions

--- tiktok/src/Content.js
+++ tiktok/src/Content.js
@@ -11,74 +11,21 @@ import { useEffect, useState } from "react";
 //-----------------
 //1. Callback luôn được gọi sau khi component mounted
 
-const tabs = ["posts", "comments", "albums"];
-
 function Content() {
-  const [posts, setPosts] = useState([]);
-  const [type, setType] = useState(["posts"]);
-  const [showGoToTop, setShowGoToTop] = useState(false);
+  const [countdown, setCountdown] = useState(180);
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${type}`)
-      .then((res) => res.json())
-      .then((posts) => {
-        setPosts(posts);
-      });
-  }, [type]);
+    const timerId = setTimeout(() => {
+      setCountdown(countdown - 1);
+      console.log('Countdown...');
+    }, 1000);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY >= 200) {
-        setShowGoToTop(true);
-      } else {
-        setShowGoToTop(false);
-      }
-
-      //setShowGoToTop(window.scrollY >= 200)
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    //Cleanup funtion
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+    return() => clearInterval(timerId)
+  }, [countdown]);
 
   return (
     <div>
-      {tabs.map((tab) => (
-        <button
-          key={tab}
-          style={
-            type === tab
-              ? {
-                  color: "#fff",
-                  backgroundColor: "#333",
-                }
-              : {}
-          }
-          onClick={() => setType(tab)}
-        >
-          {tab}
-        </button>
-      ))}
-
-      {posts.map((post) => (
-        <li key={post.id}>{post.title}</li>
-      ))}
-
-      {showGoToTop && (
-        <button
-          style={{
-            position: "fixed",
-            right: 20,
-            bottom: 20,
-          }}
-        >
-          Go to Top
-        </button>
-      )}
+      <h1>{countdown}</h1>
     </div>
   );
 }
