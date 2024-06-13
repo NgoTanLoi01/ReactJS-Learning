e7d9270fe40b121ea9f954e237b6bc392fbe6844
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-06-13 11:30:06 +0700

    useRef  hook

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,15 +1,46 @@
-import { useState } from "react";
-import Content from "./Content";
+import { useRef, useState, useEffect } from "react";
 
-//Mounted / Unmounted
+// Lưu các giá trị qua một tham chiếu bên ngoài
+// function component
 
 function App() {
-  const [show, setShow] = useState(false)
+  const [count, setCount] = useState(60);
+
+  const timerId = useRef();
+  const prevCount = useRef();
+  const h1Ref = useRef();
+
+  useEffect(() => {
+    prevCount.current = count;
+  }, [count]);
+
+  useEffect(()=>{
+    const rect = h1Ref.current.getBoundingClientRect()
+
+    console.log(rect);
+  })
+
+  const handleStart = () => {
+    timerId.current = setInterval(() => {
+      setCount((prevCount) => prevCount - 1);
+    }, 1000);
+
+    console.log("Start ->", timerId.current);
+  };
+
+  const handleStop = () => {
+    clearInterval(timerId.current);
+
+    console.log("Stop ->", timerId.current);
+  };
+
+  console.log(count, prevCount.current);
 
   return (
     <div style={{ padding: 20 }}>
-      <button onClick={() =>setShow(!show)}>Toggle</button>
-      {show && <Content />}
+      <h1 ref={h1Ref}>{count}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
     </div>
   );
 }
