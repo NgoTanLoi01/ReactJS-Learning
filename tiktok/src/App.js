e75ad1119f77602da7ba04a6468c91b2caef32f6
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-06-15 22:49:25 +0700

    useMemo() hook

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,22 +1,57 @@
-import { useState, useCallback } from "react";
-import Content from "./Content";
-
-// 1. memo() -> Higher Order Component (HOC)
-// 2. useCallback()
-// - Reference types
-// - React memo()
+import { useState, useMemo, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [products, setProducts] = useState([]);
 
-  const handleIncrease = useCallback(() => {
-    setCount((prevCount) => prevCount + 1);
-  }, []);
+  const nameRef = useRef()
+
+  const handleSubmit = () => {
+    setProducts([...products, {
+      name, 
+      price: +price
+    }])
+    setName('')
+    setPrice('')
+
+    nameRef.current.focus()
+  }
+
+  const total = useMemo(()=>{
+    const result = products.reduce((result, prod) =>{
+
+      console.log('Tính toán lại');
+
+      return result + prod.price
+    }, 0)
+
+    return result
+  }, [products])
   
   return (
     <div style={{ padding: "10px 32px" }}>
-      <Content onIncrease={handleIncrease} />
-      <h1>{count}</h1>
+      <input 
+        ref={nameRef}
+        value={name}
+        placeholder="Enter name..."
+        onChange={e => setName(e.target.value)}
+      />  
+      <br />
+      <input 
+        value={price}
+        placeholder="Enter price..."
+        onChange={e => setPrice(e.target.value)}
+      />  
+      <br />
+      <button onClick={handleSubmit}>Add</button>
+      <br />
+      Total: {total}
+      <ul>
+        {products.map((product, index) =>(
+          <li key={index}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
